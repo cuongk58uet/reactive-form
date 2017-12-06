993a1342f6a8a580353d67b630afb09fafeba4f6
Nguyen Manh Cuong <cuongnm@rikkeisoft.com>
2017-12-06 18:49:44 +0700

    Reactive form

---
+++ src/app/hero-detail.component.js
@@ -0,0 +1,52 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
+var data_model_1 = require("./data-model");
+var HeroDetailComponent = (function () {
+    function HeroDetailComponent(fb) {
+        this.fb = fb;
+        this.states = data_model_1.states;
+        this.createForm();
+    }
+    HeroDetailComponent.prototype.ngOnChanges = function () {
+        this.heroForm.reset({
+            name: this.hero.name,
+            address: this.hero.addresses[0] || new data_model_1.Address()
+        });
+        this.heroForm.setValue({
+            name: this.hero.name,
+            address: this.hero.addresses[0] || new data_model_1.Address()
+        });
+    };
+    HeroDetailComponent.prototype.createForm = function () {
+        this.heroForm = this.fb.group({
+            name: ['', forms_1.Validators.required],
+            address: this.fb.group(new data_model_1.Address()),
+            power: '',
+            sidekick: '',
+        });
+    };
+    return HeroDetailComponent;
+}());
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", data_model_1.Hero)
+], HeroDetailComponent.prototype, "hero", void 0);
+HeroDetailComponent = __decorate([
+    core_1.Component({
+        selector: 'hero-detail',
+        templateUrl: './hero-detail.component.html'
+    }),
+    __metadata("design:paramtypes", [forms_1.FormBuilder])
+], HeroDetailComponent);
+exports.HeroDetailComponent = HeroDetailComponent;
+//# sourceMappingURL=hero-detail.component.js.map
---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Address, Hero, states } from './data-model';
+
+@Component({
+  selector: 'hero-detail',
+  templateUrl: './hero-detail.component.html'
+})
+
+export class HeroDetailComponent implements OnChanges {
+  @Input() hero: Hero;
+  heroForm: FormGroup;
+  states = states;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  ngOnChanges() {
+    this.heroForm.reset({
+      name: this.hero.name,
+      address: this.hero.addresses[0] || new Address()
+    });
+    this.heroForm.setValue({
+      name: this.hero.name,
+      address: this.hero.addresses[0] || new Address()
+    });
+  }
+
+  createForm() {
+    this.heroForm = this.fb.group({
+      name: ['', Validators.required ],
+      address: this.fb.group(new Address()),
+      power: '',
+      sidekick: '',
+    });
+  }
+}
